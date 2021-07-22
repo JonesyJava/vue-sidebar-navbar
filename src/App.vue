e4a280585698b2639ed76513cfd17147782d16cb
Jonesy <jonesyjava@gmail.com>
2021-07-22 11:58:00 -0600

    vue-sidebar-navbar project init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <SidebarNavbar />
+  </header>
+  <main :style="{ 'margin-left': sidebarWidth }">
+    <router-view />
+  </main>
+  <!-- <footer>
+    <div class="bg-dark text-light text-center p-4">
+      Made with 💖 by CodeWorks
+    </div>
+  </footer> -->
+</template>
+
+<script>
+import SidebarNavbar from './components/SidebarNavbar.vue'
+import { sidebarWidth } from './components/state'
+export default {
+  components: { SidebarNavbar },
+  name: 'App',
+  setup() {
+    return {
+      sidebarWidth
+
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+</style>
---
+++ src/components/SidebarLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <router-link :to="to" class="link" :class="{ active: isActive }">
+    <i class="icon" :class="icon" />
+    <span v-if="!collapsed">
+      <slot />
+    </span>
+  </router-link>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { collapsed } from './state'
+export default {
+  name: 'SidebarLink',
+  props: {
+    to: { type: String, required: true },
+    icon: { type: String, required: true }
+  },
+  setup(props) {
+    const route = useRoute()
+    const isActive = computed(() => route.path === props.to)
+    return { isActive, collapsed }
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.link {
+  display: flex;
+  align-items: center;
+}
+</style>
